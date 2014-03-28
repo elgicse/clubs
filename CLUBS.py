13c4bd10f0d834596b2f1678b8cfa29fcce9fb88
elgicse <elena.graverini@gmail.com>
2014-03-28 20:28:57 +0100

    New branch for square matrices of 0 and 1

--- CLUBS.py
+++ CLUBS.py
@@ -20,6 +20,7 @@
 
 import numpy as np
 import sys
+from random import randint
 
 dataSet = None
 ndim = None
@@ -53,8 +54,8 @@ class newCluster():
 			# The first two parameters are the edges of the cluster
 			self.limitsLow = limitsLow
 			self.limitsHigh = limitsHigh
-			self.weight = computeWeightIn(limitsLow, limitsHigh)
-			self.Sum = computeSum(limitsLow, limitsHigh)
+			self.weight = computeWeightIn(self.limitsLow, self.limitsHigh)
+			self.Sum = computeSum(self.limitsLow, self.limitsHigh)
 			self.sqSum = [0]*ndim
 			self.CoG = [None]*ndim
 			self.SSQ = 0
@@ -71,8 +72,9 @@ class newCluster():
 	def computeSqSum(self):
 		# For each dimension, compute sum of square coordinates
 		keys = keylist
+		self.sqSum = list(self.sqSum)
 		for i in xrange(ndim):
-			keys = [key for key in keys if key[i] > limitsLow[i] and key[i] < limitsHigh[i]]
+			keys = [key for key in keys if key[i] >= self.limitsLow[i] and key[i] <= self.limitsHigh[i]]
 		for i in xrange(ndim):
 			for key in keys:
 				# A bin with weight W counts as W times that bin
@@ -96,22 +98,32 @@ class newPriorityQueue():
 	""" Selects the cluster with the highest SSQ """
 	def __init__(self):
 		self.queue = []
+		self.listOfClusters = []
 	def add(self, node):
 		self.queue.append((node, node.clusterData.computeSSQ()))
 	def get(self):
 		# Returns the cluster with the highest SSQ
 		self.queue = sorted(self.queue, key = lambda x: x[1])
-		return self.queue[-1][0]
+		return self.queue[-1]
 	def delete(self, cl):
 		self.queue.remove(cl)
+	def makeListOfClusters(self):
+		# For the bottom-up part of the algorithm
+		self.listOfClusters = [item[0].clusterData for item in self.queue]
+		print self.listOfClusters
+	def addCluster(self, cl):
+		self.listOfClusters.append(cl)
+	def deleteCluster(self, cl):
+		self.listOfClusters.remove(cl)
 
 def computeWeightIn(limitsLow, limitsHigh):
 	""" Compute content of cluster """
 	keys = keylist
 	weight = 0
 	for i in xrange(ndim):
-		keys = [key for key in keys if key[i] > limitsLow[i] and key[i] < limitsHigh[i]]
+		keys = [key for key in keys if key[i] >= limitsLow[i] and key[i] <= limitsHigh[i]]
 	for key in keys:
+		#print key, dataSet[key]
 		weight += dataSet[key]
 	return weight
 
@@ -120,7 +132,7 @@ def computeSum(limitsLow, limitsHigh):
 	keys = keylist
 	vecSum = [0]*ndim
 	for i in xrange(ndim):
-		keys = [key for key in keys if key[i] > limitsLow[i] and key[i] < limitsHigh[i]]
+		keys = [key for key in keys if key[i] >= limitsLow[i] and key[i] <= limitsHigh[i]]
 	for i in xrange(ndim):
 		for key in keys:
 			# A bin with weight W counts as W times that bin
@@ -131,6 +143,7 @@ def computeSum(limitsLow, limitsHigh):
 def computeDeltaSSQ(c1, c2):
 	""" Variation in SSQ due to the splitting of a cluster or to the merging of two sub-clusters"""
 	dssq = 0
+	#print c1.weight, c2.weight
 	factor = (c1.weight * c2.weight) / (c1.weight + c2.weight)
 	for i in xrange(ndim):
 		dssq += pow( c1.Sum[i]/c1.weight - c2.Sum[i]/c2.weight, 2 )
@@ -174,6 +187,8 @@ def findClustersToMerge():
 	""" Find the pair of adjacent clusters that yiealds the least SSQ increase """
 	global listOfMergeablePairs
 	listOfMergeablePairs = sorted(listOfMergeablePairs, key = lambda x: x[2])
+	if len(listOfMergeablePairs) is 0:
+		return False
 	bestPair = listOfMergeablePairs[0]
 	return bestPair[0], bestPair[1]
 
@@ -201,11 +216,13 @@ def splitCluster(cl, avgDeltaSSQ):
 			left = newCluster(leftLimitsLow, leftLimitsHigh)
 			right = newCluster(rightLimitsLow, rightLimitsHigh)
 			# Compute weighted Delta SSQ
-			newDssq = pow(computeDeltaSSQ(left, right), power)
-			if newDssq > refDssq:
-				# Look for the maximum weighted Delta SSQ
-				refDssq = newDssq
-				subClusters = [left, right]
+			#print key
+			if left.weight>0 and right.weight>0:
+				newDssq = pow(computeDeltaSSQ(left, right), power)
+				if newDssq > refDssq:
+					# Look for the maximum weighted Delta SSQ
+					refDssq = newDssq
+					subClusters = [left, right]
 	if refDssq > avgDeltaSSQ:
 		return subClusters
 	else:
@@ -229,46 +246,53 @@ def initStructures():
 	# Initialize the root of the binary tree as the full data set
 	root = newTreeNode(marginsLow, marginsHigh)
 	pq.add(root)
-	avgDeltaSSQ = root.clusterData.computeSSQ() / root.clusterData.weight
+	#avgDeltaSSQ = root.clusterData.computeSSQ() / root.clusterData.weight
+	avgDeltaSSQ = root.clusterData.computeSSQ() / np.sqrt(len(keylist))
 	return True
 
 def topDownSplitting():
 	""" Split the data set into micro-clusters """
 	global pq, done
 	while not done:
-		currentCluster = pq.get().clusterData
+		currentNode = pq.get()
+		currentCluster = currentNode[0].clusterData
 		children = splitCluster(currentCluster, avgDeltaSSQ)
 		if not children:
 			done = True
 		else:
-			currentCluster.setChildren(children)
-			pq.delete(currentCluster)
-			pq.add(currentCluster.left)
-			pq.add(currentCluster.right)
+			currentNode[0].setChildren(children)
+			pq.delete(currentNode)
+			pq.add(currentNode[0].left)
+			pq.add(currentNode[0].right)
+	print "Data set split into %s micro-clusters."%len(pq.queue)
 	return True
 
 def bottomUpClustering():
 	""" Merge micro-clusters to provide the final clustering """
-	global listOfMergeablePairs
-	for c1 in pq:
-		for c2 in pq:
+	global listOfMergeablePairs, pq
+	pq.makeListOfClusters()
+	for c1 in pq.listOfClusters:
+		for c2 in pq.listOfClusters:
 			if (c2 is not c1) and areAdjacent(c1, c2):
 				dssq = computeDeltaSSQ(c1, c2)
 				listOfMergeablePairs.append( (c1, c2, dssq) )
 	clustersToMerge = findClustersToMerge() # a pair of two clusters
+	if not clustersToMerge:
+		return True
 	minSSQincrease = computeDeltaSSQ(clustersToMerge)
 	while minSSQincrease < avgDeltaSSQ:
 		largerCluster = mergeClusters(clustersToMerge)
-		listOfMergeablePairs = [item for item in listOfMergeablePairs if item[0] is clustersToMerge[0] or item[0] is clustersToMerge[1] or item[1] is clustersToMerge[0] or item[1] is clustersToMerge[1]]
-		pq.delete(clustersToMerge[0])
-		pq.delete(clustersToMerge[1])
-		pq.add(largerCluster)
-		for cl in pq:
+		listOfMergeablePairs = [item for item in listOfMergeablePairs if item[0] is not clustersToMerge[0] and item[0] is not clustersToMerge[1] and item[1] is not clustersToMerge[0] and item[1] is not clustersToMerge[1]]
+		pq.deleteCluster(clustersToMerge[0])
+		pq.deleteCluster(clustersToMerge[1])
+		pq.addCluster(largerCluster)
+		for cl in pq.listOfClusters:
 			if (cl is not largerCluster) and areAdjacent(largerCluster, cl):
 				dssq = computeDeltaSSQ(largerCluster, cl)
 				listOfMergeablePairs.append( (largerCluster, cl, dssq) )
 		clustersToMerge = findClustersToMerge()
 		minSSQincrease = computeDeltaSSQ(clustersToMerge)
+	print "Micro-clusters regrouped into %s clusters."%len(pq.listOfClusters)
 	return True
 
 def findClusters():
@@ -276,22 +300,41 @@ def findClusters():
 	if not initStructures():
 		print "Error: initStructures"
 		sys.exit(1)
+	#sys.exit(0)
 	if not topDownSplitting():
 		print "Error: topDownSplitting"
 		sys.exit(1)
 	if not bottomUpClustering():
 		print "Error: bottomUpClustering"
 		sys.exit(1)
-	return pq
+	# Return the centers of gravity of found clusters
+	listOfCoGs = []
+	for cl in pq.listOfClusters:
+		listOfCoGs.append(cl.computeCoG())
+	return listOfCoGs
 
-def CLUBSclustering(dataSet, ndim):
+def CLUBSclustering(data, nd):
 	""" Execute findClusters() and handle I/O """
 	global dataSet, ndim, power, pq, done, avgDeltaSSQ, listOfMergeablePairs
-	dataSet = dataSet
-	ndim = ndim
+	dataSet = data
+	ndim = nd
 	power = 0.8
 	pq = newPriorityQueue()
 	done = False
 	avgDeltaSSQ = 0
 	listOfMergeablePairs = []
 	return findClusters()
+
+	
+if __name__ == '__main__':
+	"""sample usage"""
+	dim = 50
+	DS = dict([((x,y),0) for x in range(dim) for y in range(dim)])	
+	for i in range(1800):
+	    DS[(randint(30,dim-1),randint(0,dim-1))] = randint(0,20)
+	for i in range(700):
+	    DS[(randint(0,20),randint(0,dim-1))] = randint(0,20)
+	clusters = CLUBSclustering(DS,2)
+	print clusters
+
+
